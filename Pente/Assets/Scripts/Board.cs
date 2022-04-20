e95e9ab29250b0d9cca9dfeaf6c55c698ba2cf6b
hababa2 <zactanpeterson@gmail.com>
2022-04-20 12:36:10 -0600

    Created Board class

---
+++ Pente/Assets/Scripts/Board.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+	int[,] board;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
